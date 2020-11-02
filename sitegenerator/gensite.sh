d7036e1a085b92261ccac65f8e5544c6cd8d488d
Derek <derek7mc@gmail.com>
2020-11-02 01:25:13 -0700

    renamed site generator folder

--- sitegenerator/gensite.sh
+++ sitegenerator/gensite.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 
-rm -r output/*
+# rm -r output/*
 
 #echo 'hey'
 find content/ -type f -name "*.md" |
-  sed -e 's/content\/\(.*\)\.md/\0 output\/\1.html/g' |
+  sed -e 's/content\/\(.*\)\.md/\0 ..\/\1.html/g' |
   while read infile outfile; do
     #./md2html $infile > $outfile;
     ./md2html $infile | cat template/header.html - template/footer.html > $outfile
 done
 
 
-cp -r static/* output
+cp -r static/* ../
 #find static/ -type f |
   #set -e 's/static\/\(.*\)/\0 output\/\1/g' |
   #while read $from $to; do
